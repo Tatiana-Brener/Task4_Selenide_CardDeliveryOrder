5d4d4b393364eaafc2c43be66cead3c7c3430ef4
Tatana <burmatova@inbox.ru>
2021-02-07 17:27:27 +0300

    Added tests

---
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -0,0 +1,113 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryOrderTest {
+    LocalDate dataOfMeeting = LocalDate.now().plusDays(3);
+    String inputData = dataOfMeeting.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    @Test
+    void shouldDeliverCardFormAllFieldsFilledCorrectly() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Казань");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=notification] .notification__content").shouldBe(Condition.visible, Duration.ofMillis(15000)).shouldHave(Condition.exactText("Встреча успешно забронирована на " + inputData));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfFieldCityIsEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=city] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfCityIsNotFromRF() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("City");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=city] .input__sub").shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfFieldNameIsIncorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Ivan Petrov");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfFieldNameIsEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfFieldPhoneIsIncorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("89009000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfFieldPhoneIsEmpty() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotDeliverCardFormIfCheckboxNotClic() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[placeholder='Дата встречи']").setValue(inputData);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("");
+        $("[data-test-id='agreement'] .checkbox__box");
+        $(".grid-col button").click();
+        $("[data-test-id='agreement'] .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+}
