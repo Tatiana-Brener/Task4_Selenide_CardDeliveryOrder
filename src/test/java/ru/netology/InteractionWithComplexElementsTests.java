2e90be26c3b7eff72988b770dcf88f80279b1860
Tatana <burmatova@inbox.ru>
2021-02-22 19:03:23 +0300

    my attempt to find decision of a task with calendar

--- src/test/java/ru/netology/InteractionWithComplexElementsTests.java
+++ src/test/java/ru/netology/InteractionWithComplexElementsTests.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
@@ -65,6 +68,32 @@ public class InteractionWithComplexElementsTests {
         $("[data-test-id=notification] .notification__content").
                 shouldBe(Condition.visible, Duration.ofMillis(15000)).
                 shouldHave(Condition.exactText("Встреча успешно забронирована на " + inputData));
+    }
+
+//     Это последняя попытка решить задачу
+    @Test
+    void shouldDeliverCardFormIfDateOfMeetingIsChooseUsingCalendarThisMounth2() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Са");
+        $$(" .menu-item__control").find(Condition.exactText("Санкт-Петербург")).click();
+
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $(".calendar-input__custom-control [type=button]").click();
+        SelenideElement block = $(".popup__container .calendar");
+        Calendar dataOfMeeting = new GregorianCalendar(Calendar.DAY_OF_MONTH, Calendar.MONTH, Calendar.YEAR);
+//        Calendar dataOfMeeting = Calendar.getInstance();
+        $$(".popup__container .calendar .calendar__day");
+        dataOfMeeting.add(Calendar.DAY_OF_MONTH, 5);
+        dataOfMeeting.add(Calendar.MONTH, 1);
+        dataOfMeeting.add(Calendar.YEAR, 0);
+
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров");
+        $("[data-test-id='phone'] .input__control").setValue("+79009990000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".grid-col button").click();
+        $("[data-test-id=notification] .notification__content").
+                shouldBe(Condition.visible, Duration.ofMillis(15000)).
+                shouldHave(Condition.exactText("Встреча успешно забронирована на " + dataOfMeeting));
     }
 
     @Test
